13e69d7e7834e8f30696a288f07e13e89fc65188
zjubird <pz@pdeMacBook-Pro.local>
2018-05-27 13:50:12 +0800

    solution

--- 1_two_sum.go
+++ 1_two_sum.go
@@ -1,1 +1,18 @@
+package main
+import "fmt"
 
+func twoSum(s []int, target int) (int, int){
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++{
+			if s[i] + s[j] == target{
+				return i,j
+			}
+		}
+	}
+	return 0,0
+}
+
+func main(){
+	data := []int{1,2,3,4,5}
+	fmt.Println(twoSum(data, 9))
+}
